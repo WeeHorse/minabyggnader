f672df0eccd0dcec84dbab11d89d5f7066cd2888
WeeHorse <benjaminberglund@gmail.com>
2016-02-10 13:29:25 +0100

    first commit for plugin

---
+++ wp-content/plugins/mina_byggnader_data_plugin/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Mina Byggnader Data Plugin
+ * @version 1.0
+ */
+/*
+Plugin Name: Mina Byggnader Data Plugin
+Description: Delivers a file structure to the client (HTML/JS).
+Author: Benjamin Berglund
+Version: 1.0
+*/
+
